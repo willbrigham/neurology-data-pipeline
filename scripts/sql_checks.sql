a272da9f012db587780bb349a4a299cf0c94ed8c
Will <wibrigham9890@gmail.com>
2025-08-26 23:50:31 -0400

    Remove SQL Checks

--- scripts/sql_checks.sql
+++
@@ -1,9 +0,0 @@
-SELECT TOP 5 * FROM stg.Taxonomy;
-SELECT TOP 5 * FROM stg.Addresses;
-
--- Counts
-SELECT COUNT(*) AS tax_cnt FROM stg.Taxonomy;
-SELECT COUNT(*) AS addr_cnt FROM stg.Addresses;
-
--- Neurologists present?
-SELECT COUNT(*) FROM stg.Taxonomy WHERE taxonomy_code='2084N0400X';
